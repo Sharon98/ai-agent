56beeac87aa84aa21dc29aba3fb50b1920ad9cae
Sharon98 <sharon@arana-m.com>
2025-06-09 11:53:20 -0500

    making google-ai-agent

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
+meta = response.usage_metadata
+print(f"Prompt tokens: {meta.prompt_token_count}")
+print(f"Response tokens: {meta.candidates_token_count}")
